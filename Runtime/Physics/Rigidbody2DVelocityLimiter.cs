21d8132800044d2fc8459f5106684b6ba826bcab
Vasiliy <catavasya@ya.ru>
2024-08-06 14:08:44 +0300

    Update Rigidbody2DVelocityLimiter.cs

--- Runtime/Physics/Rigidbody2DVelocityLimiter.cs
+++ Runtime/Physics/Rigidbody2DVelocityLimiter.cs
@@ -21,12 +21,12 @@
         public void FixedUpdate()
         {
 
-            if (UseMaxValocity && Rigidbody2D.linearVelocity.sqrMagnitude > _sqrMaxValocity)
+            if (UseMaxValocity && Rigidbody2D.velocity.sqrMagnitude > _sqrMaxValocity)
             {
                 Rigidbody2D.velocity = Rigidbody2D.velocity.normalized * MaxVelocity;
             }
 
-            if (UseMinValocity && Rigidbody2D.linearVelocity.sqrMagnitude < _sqrMinVelocity)
+            if (UseMinValocity && Rigidbody2D.velocity.sqrMagnitude < _sqrMinVelocity)
             {
                 Rigidbody2D.velocity = Rigidbody2D.velocity.normalized * MinVelocity;
             }
